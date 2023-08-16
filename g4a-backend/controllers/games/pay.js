33b6a480be2fb87e076b4f1acc18cc3ab508040b
Samuel García <120742240+Samuel0273@users.noreply.github.com>
2023-08-16 11:28:46 -0500

    Update pay.js

--- g4a-backend/controllers/games/pay.js
+++ g4a-backend/controllers/games/pay.js
@@ -1,7 +1,6 @@
-const express = require('express');
-const stripe = require("stripe")(
-  "sk_test_51NfjIYGeX3wi2NjaFBArkYpXGhmP2Um7sRqWrxlwHvg6JNN3RE16noGDAwZrj5agKk5SMN8tHzFxfXHGmWVfIIFM00UPBTIjAB"
-);
+import stripe from "stripe";
+const stripeSecretKey = "sk_test_51NfjIYGeX3wi2NjaFBArkYpXGhmP2Um7sRqWrxlwHvg6JNN3RE16noGDAwZrj5agKk5SMN8tHzFxfXHGmWVfIIFM00UPBTIjAB";
+const stripeInstance = new stripe(stripeSecretKey);
 
 export default async (req, res, next) => {
   try {
